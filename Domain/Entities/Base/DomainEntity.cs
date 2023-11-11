92238e9ab940f89b7a19543bc1a21978efd2b49c
dvillcar <diego.villa@devzeros.com>
2023-11-11 16:03:01 -0500

    ajuste para validar fallo

--- Domain/Entities/Base/DomainEntity.cs
+++ Domain/Entities/Base/DomainEntity.cs
@@ -3,6 +3,8 @@ namespace Domain.Entities.Base
     public class DomainEntity : ISoftDelete
     {
         public DateTime? DeletedOn { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime LastModifiedOn { get; set; }
 
         public void SetDelete() => DeletedOn = DateTime.Now;
     }
