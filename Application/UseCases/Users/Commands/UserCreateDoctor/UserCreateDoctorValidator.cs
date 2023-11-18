24d138cb678070872e60ca605c08acee355eda6c
Heli Cadena <heli.cadena@devzeros.com>
2023-11-18 08:31:14 -0500

    changes

--- Application/UseCases/Users/Commands/UserCreateDoctor/UserCreateDoctorValidator.cs
+++ Application/UseCases/Users/Commands/UserCreateDoctor/UserCreateDoctorValidator.cs
@@ -13,10 +13,10 @@ public class UserCreateDoctorValidator : AbstractValidator<UserCreateDoctorComma
         RuleFor(c => c.Doctor.LastName).NotNull().MinimumLength(2).MaximumLength(40);
         RuleFor(c => c.Doctor.SecondLastName).NotNull().MinimumLength(2).MaximumLength(40);
         RuleFor(c => c.Doctor.DocumentType).NotNull();
-        RuleFor(c => c.Doctor.DocumentNumber).NotNull();
-        RuleFor(c => c.Doctor.Email).NotNull();
-        RuleFor(c => c.Doctor.Phone).NotNull();
-        RuleFor(c => c.Doctor.Address).NotNull();
+        RuleFor(c => c.Doctor.DocumentNumber).NotNull().MinimumLength(6).MaximumLength(10);
+        RuleFor(c => c.Doctor.Email).NotNull().MinimumLength(4).MaximumLength(40);
+        RuleFor(c => c.Doctor.Phone).NotNull().MinimumLength(6).MaximumLength(10);
+        RuleFor(c => c.Doctor.Address).NotNull().MaximumLength(40);
         RuleFor(c => c.Doctor.Birthdate).NotNull();
         RuleFor(c => c.Doctor.Specialization).NotNull();
     }
