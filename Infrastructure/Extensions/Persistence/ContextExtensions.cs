89607edd3f4dced3a809759012825560033f2577
Victor Venegas <victorvenegas07@gmail.com>
2023-11-26 23:25:49 -0500

    Pomelo appsentings dev

--- Infrastructure/Extensions/Persistence/ContextExtensions.cs
+++ Infrastructure/Extensions/Persistence/ContextExtensions.cs
@@ -9,14 +9,21 @@ public static class ContextExtensions
 {
     public static IServiceCollection AddContextDatabase(this IServiceCollection svc, IConfiguration config)
     {
-        svc.AddDbContext<PersistenceContext>(opt =>
-        {
-            opt.UseSqlServer(config.GetConnectionString("database"),
-                sqlopts =>
-                {
-                    sqlopts.MigrationsHistoryTable("_MigrationHistory", config.GetValue<string>("SchemaName"));
-                });
-        });
+        //svc.AddDbContext<PersistenceContext>(opt =>
+        //{
+        //    opt.UseSqlServer(config.GetConnectionString("database"),
+        //        sqlopts =>
+        //        {
+        //            sqlopts.MigrationsHistoryTable("_MigrationHistory", config.GetValue<string>("SchemaName"));
+        //        });
+        //});
+
+        svc.AddDbContext<PersistenceContext>(options =>
+        options.UseMySql(
+              config.GetConnectionString("DefaultConnection"),
+              new MySqlServerVersion(new Version(8, 0, 26)),
+              b => b.MigrationsAssembly(typeof(PersistenceContext).Assembly.FullName)),
+              ServiceLifetime.Transient);
         return svc;
     }
 }
