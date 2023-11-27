a7603767693acf2425f56ef9baa963f1ec9c96ed
Heli Cadena <heli.cadena@devzeros.com>
2023-11-27 00:32:11 -0500

    context update

--- Infrastructure/Extensions/Persistence/ContextExtensions.cs
+++ Infrastructure/Extensions/Persistence/ContextExtensions.cs
@@ -9,21 +9,14 @@ public static class ContextExtensions
 {
     public static IServiceCollection AddContextDatabase(this IServiceCollection svc, IConfiguration config)
     {
-        //svc.AddDbContext<PersistenceContext>(opt =>
-        //{
-        //    opt.UseSqlServer(config.GetConnectionString("database"),
-        //        sqlopts =>
-        //        {
-        //            sqlopts.MigrationsHistoryTable("_MigrationHistory", config.GetValue<string>("SchemaName"));
-        //        });
-        //});
-
-        svc.AddDbContext<PersistenceContext>(options =>
-        options.UseMySql(
-              config.GetConnectionString("DefaultConnection"),
-              new MySqlServerVersion(new Version(8, 0, 26)),
-              b => b.MigrationsAssembly(typeof(PersistenceContext).Assembly.FullName)),
-              ServiceLifetime.Transient);
+        svc.AddDbContext<PersistenceContext>(opt =>
+        {
+            opt.UseSqlServer(config.GetConnectionString("database"),
+                sqlopts =>
+                {
+                    sqlopts.MigrationsHistoryTable("_MigrationHistory", config.GetValue<string>("SchemaName"));
+                });
+        });
         return svc;
     }
 }
